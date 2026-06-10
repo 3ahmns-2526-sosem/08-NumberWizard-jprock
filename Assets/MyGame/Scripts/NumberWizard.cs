428b9c3efae42fb492fd56587284e70ed14bf158
jakobprock <jakob.prock09@htl-salzburg.ac.at>
2026-06-10 14:34:30 +0200

    feat: calculate first guess close #3

---
+++ Assets/MyGame/Scripts/NumberWizard.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    public int min = 1;
+    public int max = 100;
+    private int guess;
+
+    public TextMeshProUGUI guessText;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+    }
+}
